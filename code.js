0340ab395e9754216ab75d618dd5db8211e9083d
reinabd <reinabd@proton.me>
2026-08-31 23:33:57 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,123 @@
+
+
+
+
+
+
+
+setTimeout(() => {
+   
+const now = new Date();
+const month = now.getMonth();
+const day = now.getDate();
+const year = now.getFullYear();
+const age = year - 2012 
+if (month == 9 && day == 8) {
+    if (age == 14) { 
+    window.location.href = "https://reinabd.github.io/happybirthday14/";
+    }
+    else if (age == 15) { 
+    window.location.href = "https://reinabd.github.io/happybirthday15/";
+    }
+    else if (age == 16) { 
+    window.location.href = "https://reinabd.github.io/happybirthday16/";
+    }
+    else if (age == 17) { 
+    window.location.href = "https://reinabd.github.io/happybirthday17/";
+    }
+    else if (age == 18) { 
+    window.location.href = "https://reinabd.github.io/happybirthday18/";
+    } 
+    else if (age == 19) { 
+    window.location.href = "https://reinabd.github.io/happybirthday19/";
+    }
+    else if (age == 20) { 
+    window.location.href = "https://reinabd.github.io/happybirthday20/";
+    }
+    else if (age == 21) { 
+    window.location.href = "https://reinabd.github.io/happybirthday21/";
+    }
+    else if (age == 22) { 
+    window.location.href = "https://reinabd.github.io/happybirthday22/";
+    }
+    else if (age == 23) { 
+    window.location.href = "https://reinabd.github.io/happybirthday23/";
+    }
+    else if (age == 24) { 
+    window.location.href = "https://reinabd.github.io/happybirthday24/";
+    }
+    else if (age == 25) { 
+    window.location.href = "https://reinabd.github.io/happybirthday25/";
+    }
+    else if (age == 26) { 
+    window.location.href = "https://reinabd.github.io/happybirthday26/";
+    }
+    else {
+    window.location.href = "https://reinabd.github.io/happybirthday/";
+    }
+
+
+}
+
+
+else if ( new Date(` October 09 , 2025 00:00:00 `)<= now && now < new Date(` October 08 , 2026 00:00:00 `)){ 
+window.location.href = "https://reinabd.github.io/0";
+}
+else if ( new Date(` October 09 , 2026 00:00:00 `)<= now && now < new Date(` October 08 , 2027 00:00:00 `)){ 
+window.location.href = "https://reinabd.github.io/1";
+}
+else if ( new Date(` October 09 , 2027 00:00:00 `)<= now && now < new Date(` October 08 , 2028 00:00:00 `)){ 
+window.location.href = "https://reinabd.github.io/2";
+}
+else if ( new Date(` October 09 , 2028 00:00:00 `)<= now && now < new Date(` October 08 , 2029 00:00:00 `)){ 
+window.location.href = "https://reinabd.github.io/3";
+}
+else if ( new Date(` October 09 , 2029 00:00:00 `)<= now && now < new Date(` October 08 , 2030 00:00:00 `)){ 
+window.location.href = "https://reinabd.github.io/4";
+}
+else if ( new Date(` October 09 , 2030 00:00:00 `)<= now && now < new Date(` October 08 , 2031 00:00:00 `)){ 
+window.location.href = "https://reinabd.github.io/5";
+}
+else if ( new Date(` October 09 , 2031 00:00:00 `)<= now && now < new Date(` October 08 , 2032 00:00:00 `)){ 
+window.location.href = "https://reinabd.github.io/6";
+}
+else if ( new Date(` October 09 , 2032 00:00:00 `)<= now && now < new Date(` October 08 , 2033 00:00:00 `)){ 
+window.location.href = "https://reinabd.github.io/7";
+}
+else if ( new Date(` October 09 , 2033 00:00:00 `)<= now && now < new Date(` October 08 , 2034 00:00:00 `)){ 
+window.location.href = "https://reinabd.github.io/8";
+}
+else if ( new Date(` October 09 , 2034 00:00:00 `)<= now && now < new Date(` October 08 , 2035 00:00:00 `)){ 
+window.location.href = "https://reinabd.github.io/9";
+}
+else if ( new Date(` October 09 , 2035 00:00:00 `)<= now && now < new Date(` October 08 , 2036 00:00:00 `)){ 
+window.location.href = "https://reinabd.github.io/10";
+}
+else if ( new Date(` October 09 , 2036 00:00:00 `)<= now && now < new Date(` October 08 , 2037 00:00:00 `)){ 
+window.location.href = "https://reinabd.github.io/11";
+}
+else if ( new Date(` October 09 , 2037 00:00:00 `)<= now && now < new Date(` October 08 , 2038 00:00:00 `)){ 
+window.location.href = "https://reinabd.github.io/12";
+}
+
+
+
+
+else {       window.location.href = "https://reinabd.github.io/age/"; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}, 1000);
